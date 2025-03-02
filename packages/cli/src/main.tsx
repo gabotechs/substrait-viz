ce1e6118e93ebda75f3f9916f7d59cd81e09ac16
gabotechs <gb.mt.me@gmail.com>
2025-03-02 20:25:54 +0100

    Improve CLI devex

--- packages/cli/src/main.tsx
+++ packages/cli/src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { SubstraitViz } from '@substrait-viz/react';
-import './main.css';
 
 const PLAN = { __substrait_plan: '' };
 
+if (import.meta.env.VITE_PLAN) {
+  PLAN.__substrait_plan = import.meta.env.VITE_PLAN;
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div className="main">
+    <div style={{ width: '100vw', height: '100vh' }}>
       <SubstraitViz plan={PLAN.__substrait_plan} />
     </div>
   </StrictMode>,
--- packages/cli/vite.config.ts
+++ packages/cli/vite.config.ts
@@ -1,18 +1,12 @@
 import { defineConfig } from 'vite';
 
 import react from '@vitejs/plugin-react-swc';
-import { libInjectCss } from 'vite-plugin-lib-inject-css';
-import tailwindcss from '@tailwindcss/vite';
 import { viteSingleFile } from 'vite-plugin-singlefile';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), libInjectCss(), tailwindcss(), viteSingleFile()],
+  plugins: [react(), viteSingleFile()],
   build: {
     outDir: 'bin',
-    emptyOutDir: false,
-    rollupOptions: {
-      external: ['src/cli.ts'],
-    },
   },
 });
