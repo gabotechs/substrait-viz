6adbdc6da3495734c4a307e9dd7b53ca38230e1b
gabotechs <gb.mt.me@gmail.com>
2025-03-09 09:29:31 +0100

    Regenerate lock file

--- packages/SubstraitVizCli/eslint.config.js
+++ packages/SubstraitVizCli/eslint.config.js
@@ -1,5 +1,5 @@
 import tseslint from 'typescript-eslint';
-import baseConfig from '../../../eslint.config.mjs';
+import baseConfig from '../../eslint.config.mjs';
 
 export default tseslint.config(
   { ignores: ['**/dist', '**/gen'] },
