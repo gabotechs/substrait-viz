0c3142099535b14f9038ff3e1497848c002ab269
gabotechs <gb.mt.me@gmail.com>
2025-03-09 08:59:54 +0100

    Move out of multiple packages

--- packages/SubstraitVizCli/main.go
+++ packages/SubstraitVizCli/main.go
@@ -14,16 +14,16 @@ import (
 	"github.com/spf13/cobra"
 )
 
-//go:embed frontend/dist/index.html
+//go:embed dist/index.html
 var indexHtml []byte
 
-//go:embed frontend/dist/index.js
+//go:embed dist/index.js
 var indexJs []byte
 
-//go:embed frontend/dist/index.css
+//go:embed dist/index.css
 var indexCss []byte
 
-//go:embed frontend/public/logo.svg
+//go:embed public/logo.svg
 var logo []byte
 
 var descriptors []string
@@ -118,7 +118,7 @@ type PackageJson struct {
 	Version string `json:"version"`
 }
 
-//go:embed frontend/package.json
+//go:embed package.json
 var packageJson []byte
 
 func mustParsePackageJson() PackageJson {
