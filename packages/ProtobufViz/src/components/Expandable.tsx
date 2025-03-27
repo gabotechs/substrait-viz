9907acf77c03de8308b1d280ad753afdd3fe6cc0
gabotechs <gb.mt.me@gmail.com>
2025-03-27 09:31:50 +0100

    Fix ref forwarding

--- packages/ProtobufViz/src/components/Expandable.tsx
+++ packages/ProtobufViz/src/components/Expandable.tsx
@@ -9,43 +9,45 @@ export interface ExpandableProps
   setExpanded: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export function Expandable({
-  expanded,
-  isExpandable,
-  children,
-  setExpanded,
-  className = '',
-  ...props
-}: ExpandableProps) {
-  const [hoverDiv, setHoverDiv] = React.useState(false);
-  const [hoverButton, setHoverButton] = React.useState(false);
-  const hover = hoverDiv || hoverButton;
-  const { background } = useTheme();
+export const Expandable = React.forwardRef<HTMLDivElement, ExpandableProps>(
+  (
+    { expanded, isExpandable, children, setExpanded, className = '', ...props },
+    ref,
+  ) => {
+    const [hoverDiv, setHoverDiv] = React.useState(false);
+    const [hoverButton, setHoverButton] = React.useState(false);
+    const hover = hoverDiv || hoverButton;
+    const { background } = useTheme();
 
-  return (
-    <div
-      className={`${className} relative`}
-      onMouseEnter={() => setHoverDiv(true)}
-      onMouseLeave={() => setHoverDiv(false)}
-      {...props}
-    >
+    return (
       <div
-        className={`absolute top-0 cursor-pointer z-20 bg-transparent flex justify-end`}
-        style={{ opacity: !hover ? 0 : undefined, width: 'calc(100% + 16px)' }}
-        onMouseEnter={() => setHoverButton(true)}
-        onMouseLeave={() => setHoverButton(false)}
-        hidden={!isExpandable}
-        onClick={() => setExpanded(p => !p)}
+        className={`${className} relative`}
+        onMouseEnter={() => setHoverDiv(true)}
+        onMouseLeave={() => setHoverDiv(false)}
+        ref={ref}
+        {...props}
       >
-        <div className={'px-1'} style={{ background }}>
-          {expanded ? (
-            <span className={'i-mdi-arrow-collapse'} />
-          ) : (
-            <span className={'i-mdi-arrow-expand'} />
-          )}
+        <div
+          className={`absolute top-0 cursor-pointer z-20 bg-transparent flex justify-end`}
+          style={{
+            opacity: !hover ? 0 : undefined,
+            width: 'calc(100% + 16px)',
+          }}
+          onMouseEnter={() => setHoverButton(true)}
+          onMouseLeave={() => setHoverButton(false)}
+          hidden={!isExpandable}
+          onClick={() => setExpanded(p => !p)}
+        >
+          <div className={'px-1'} style={{ background }}>
+            {expanded ? (
+              <span className={'i-mdi-arrow-collapse'} />
+            ) : (
+              <span className={'i-mdi-arrow-expand'} />
+            )}
+          </div>
         </div>
+        {children}
       </div>
-      {children}
-    </div>
-  );
-}
+    );
+  },
+);
