d3b23256ea18fa19c6b31528e4c8cc28b83e670f
gabotechs <gb.mt.me@gmail.com>
2025-03-27 19:52:18 +0100

    Fix: recover the fitView call

--- packages/ProtobufViz/src/ProtobufViz.tsx
+++ packages/ProtobufViz/src/ProtobufViz.tsx
@@ -177,7 +177,7 @@ function Private<S extends MessageSchema>({
       // Let some time for the nodes to be placed.
       await new Promise(res => setTimeout(res, 10));
       setLayoutReady(true);
-      // await fitView({ duration: 400 });
+      await fitView({ duration: 400 });
     }
 
     void waitForAllNodesPlaced();
